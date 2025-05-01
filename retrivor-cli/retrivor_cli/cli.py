36c302ace716ab9f1c40af06079032cc6555e809
JVitoroliv3ira <jvitoroliveira0711@gmail.com>
2025-04-30 22:35:05 -0300

    feat: Adiciona comando ask

--- retrivor-cli/retrivor_cli/cli.py
+++ retrivor-cli/retrivor_cli/cli.py
@@ -1,9 +1,11 @@
 import typer
 from retrivor_cli.commands import index
+from retrivor_cli.commands import ask
 
 app = typer.Typer()
 
 app.add_typer(index.app)
+app.add_typer(ask.app)
 
 if __name__ == '__main__':
     app()
